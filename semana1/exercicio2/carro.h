8fc502f6cc0ada85276004872eb63e615c20385a
Karla C. Ferreira <karlinharural@gmail.com>
2023-08-03 17:24:58 -0300

    Create carro.h

---
+++ semana1/exercicio2/carro.h
@@ -0,0 +1,30 @@
+
+class Carro{
+    private:
+        string nome;
+        string marca;
+        string motorizacao;
+        string cor;
+        double valor;
+    
+    public:
+        Carro(string nome, string marca, string motorizacao, string cor, double valor);
+        
+        string getNome();
+        string getMarca();
+        string getMotorizacao();
+        string getCor();
+        double getValor();
+        
+        void setNome(string nome);
+        void setMarca(string marca);
+        void setMotorizacao(string motorizacao);
+        void setCor(string cor);
+        void setValor(double valor);
+        
+        void imprimir();
+        void preencher();
+        Carro copiar();
+};
+
+
