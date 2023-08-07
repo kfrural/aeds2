4cce43c2240d158ca018ad29f1c7c2ab7d03c30c
Karla C. Ferreira <karlinharural@gmail.com>
2023-08-07 15:19:02 -0300

    Create AnimalEstimacao.h

---
+++ semana2/animalEstimacao/AnimalEstimacao.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class AnimalEstimacao{
+    private:
+        string especie;
+        string raca;
+        int idade;
+        double peso;
+
+    public:
+    AnimalEstimacao();// construtor
+
+        string getEspecie();//metodo get pega o valor
+        string getRaca();
+        int getIdade();
+        double getPeso();
+
+        void setEspecie(string especie);
+        void setRaca(string raca);
+        void setIdade(int idade);//metodo set moifica o vaor
+        void setPeso(double peso);
+        
+        void print();
+        void fill();
+        
+};
