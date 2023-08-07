f890d1335a40fe07475c8a6075f55fcbc014c7f7
Karla C. Ferreira <karlinharural@gmail.com>
2023-08-07 15:19:34 -0300

    Create AnimalEstimacao.cpp

---
+++ semana2/animalEstimacao/AnimalEstimacao.cpp
@@ -0,0 +1,71 @@
+#include "AnimalEstimacao.h"
+
+
+AnimalEstimacao::AnimalEstimacao(){
+    
+}
+//declara o construtor e depois do :: inicializa o trem com o valor correspondente
+string AnimalEstimacao::getEspecie(){
+    return especie;
+}
+string AnimalEstimacao::getRaca(){
+    return raca;
+}
+int AnimalEstimacao::getIdade(){// definicao os metodos get pra pegar valor
+    return idade;
+}
+double AnimalEstimacao::getPeso(){
+    return peso;
+}
+
+void AnimalEstimacao::setEspecie(string especie){
+    this-> especie = especie ;
+}
+void AnimalEstimacao::setRaca(string raca){// definicao do metodo set ra mecher no valor
+    this-> raca = raca;
+}
+void AnimalEstimacao::setIdade(int idade){
+    this-> idade = idade;
+}
+void AnimalEstimacao::setPeso(double peso){
+    this->peso = peso;
+}
+
+void AnimalEstimacao::fill(){
+    cout << "Preencha abaixo:\n\n";
+    cout << "Especie: ";
+    cin >> this->especie;
+    cout << "Raca: ";
+    cin >> this->raca;
+    cout << "Idade: ";
+    cin >> this->idade;
+    cout << "Peso: ";
+    cin >> this->peso;
+}
+void AnimalEstimacao::print(){
+    cout << "\n\n\nNomezin da TVzinha\n\n";
+    cout << "Especie: " << this->especie;
+    cout << "\nRaca: " << this->raca;
+    cout << "\nIdade: " << this->idade;
+    cout << "\nPeso: " << this->peso;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
