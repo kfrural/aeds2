06a62a3eef47277fa5978ca5e6288de6931d9c3f
Karla C. Ferreira <karlinharural@gmail.com>
2023-08-07 14:22:45 -0300

    Create main.cpp

---
+++ semana2/tv/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "TV.h"
+
+using namespace std;
+
+int main()
+{
+    TV tv1;
+    tv1.fill();
+    tv1.print();
+   /* tv1.setMarca("lige");
+    tv1.setTamanho(14);
+    tv1.setResolucao("144p");
+    tv1.setPreco(800);*/
+    return 0;
+}
