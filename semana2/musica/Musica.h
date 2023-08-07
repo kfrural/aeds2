f0d6f0a9be3fc6cf591ad58c87e318252b7e25ef
Karla C. Ferreira <karlinharural@gmail.com>
2023-08-07 16:10:39 -0300

    Create Musica.h

---
+++ semana2/musica/Musica.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Musica{
+    private:
+        string titulo;
+        string artista;
+        double duracao;
+        double preco;
+
+    public:
+    Musica();// construtor
+
+        string getTitulo();//metodo get pega o valor
+        string getArtista();
+        double getDuracao();
+        double getPreco();
+
+        void setTitulo(string titulo);
+        void setArtista(string artista);
+        void setDuracao(double duracao);//metodo set moifica o vaor
+        void setPreco(double preco);
+        
+        void print();
+        void fill();
+        
+};
