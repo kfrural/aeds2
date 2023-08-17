bf875d66890b5d437f0cfce5c6a8c30a55bdef5f
Karla C. Ferreira <karlinharural@gmail.com>
2023-08-17 08:04:45 -0300

    Update main.cpp

--- semana3/cachorro/main.cpp
+++ semana3/cachorro/main.cpp
@@ -1,32 +1,10 @@
-#include <iostream>
-#include <string.h>
-#include <fstream>
+#include "Cachorro.h"
 
-using namespace std;
-
-int main()
-{
-    string caminhoPasta = "Cachorro.txt";
-    ofstream arquivoSalvo;
-    arquivoSalvo.open(caminhoPasta.c_str());
+int main(){
     
-    int id;
-    string nome;
-    char sexo;
-    int idade;
+    Cachorro cachorro;
+    cachorro.salvarArquivo();
+    cachorro.printArquivo();
     
-    cout << "Forneca as informacoes abaixo\n\n";
-    cout << "ID: ";
-    cin >> id;
-    cout << "Nome: ";
-    cin >> nome;
-    cout <<"Sexo (m/f): ";
-    cin >> sexo;
-    cout << "Idade: ";
-    cin >> idade;
-    arquivoSalvo << id << nome << sexo << idade;
-    
-    
-
     return 0;
 }
