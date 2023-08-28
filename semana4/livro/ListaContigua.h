cd442d5f846fd8d86a2346a75d64611d9a47f340
Karla C. Ferreira <karlinharural@gmail.com>
2023-08-28 13:04:07 -0300

    Create ListaContigua.h

---
+++ semana4/livro/ListaContigua.h
@@ -0,0 +1,48 @@
+#ifndef LISTALIVRO_H
+#define	LISTALIVRO_H
+
+#include "Livro.h"
+
+class ListaContigua {
+private:
+    int tam, quant;
+    Livro *lista;
+    
+    int contadorDeComparacoes;
+    
+    bool temEspaco(); 
+    bool isEmpty ();
+    bool indiceValido(int i);
+    void shiftFront(int aPartir);
+    void shiftEnd(int ate);
+    void criaListaComLivrosAleatorios(int tam);
+
+public:
+    ListaContigua();
+    ListaContigua(int tamanho);
+    ListaContigua( ListaContigua& outra);
+    ListaContigua( ListaContigua& outra, int novoTamanho);
+    virtual ~ListaContigua();
+            
+    void insert();
+    void insert(int posicao);
+    void insert(Livro p);
+    void remove();
+    void remove(int posicao);
+    
+    void imprimir();
+    void preencher();    
+    int buscar(int idLivro);    
+    Livro* buscar(double preco);
+    int buscaBinaria(int codigoProcurado);
+    
+    //GETTERS E SETTERS
+    void setLista(Livro* lista);
+    Livro* getLista() ;
+    void setQuant(int quant);
+    int getQuant() ;
+    void setTam(int tam);
+    int getTam() ;
+};
+
+#endif	/* LISTALIVRO_H */
