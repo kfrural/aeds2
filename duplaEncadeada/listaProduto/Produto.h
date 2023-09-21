2826b2a36e74d9f5d5bd8ff104cb8fb55a1db24e
Karla C. Ferreira <karlinharural@gmail.com>
2023-09-20 21:31:26 -0300

    Create Produto.h

---
+++ duplaEncadeada/listaProduto/Produto.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Produto{
+    private:
+        int id;
+        string nome;
+        double custo;
+        double preco;
+        int estoque;
+
+    public:
+        Produto();
+        Produto(Produto &outro);
+        
+        void setId(int id);
+        void setNome(string nome);
+        void setCusto(double custo);
+        void setPreco(double preco);
+        void setEstoque(int estoque);
+        
+        int getId();
+        string getNome();
+        double getCusto();
+        double getPreco();
+        int getEstoque();
+        
+        void copiar(Produto &outro);
+        void preencher();
+        void imprimir();
+        void imprimirLista();
+};
