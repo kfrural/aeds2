b6ad651dc8d7270666a4523e22dcbe2b7f8ec3d6
Karla C. Ferreira <karlinharural@gmail.com>
2023-09-28 12:04:08 +0000

    ...

--- listaEspeciais/pilhaEncadeada/PilhaDuplamenteEncadeada.cpp
+++ listaEspeciais/pilhaEncadeada/PilhaDuplamenteEncadeada.cpp
@@ -4,3 +4,51 @@ PilhaDuplamenteEncadeada::PilhaDuplamenteEncadeada(){
     quant = 0;
     head = NULL;
 }
+
+void PilhaDuplamenteEncadeada:: setQuant(int quant){
+    this->quant = quant;
+}
+int PilhaDuplamenteEncadeada:: getQuant(){
+    return quant;
+}
+
+PilhaDuplamenteEncadeada:: imprimir(){
+    cout << "\tListinha[";
+    if(isEmpty()){
+        Nodo* p = head;
+        while(p != NULL){
+            p->getItem().imprimirResumido();
+            p = p->getProx();
+        }
+        cout << "]\n";
+    }
+}
+
+PilhaDuplamenteEncadeada:: preencher(){
+    int q = 0;
+    do{
+        cout << "Quanidade de elementos: ";
+        cin >> q;
+    }while(q < 0);
+    for(int i = 0; i < q; i++){
+        this-> insert();
+    }
+}
+
+PilhaDuplamenteEncadeada:: insert(){
+    Produto p;
+    p.preencher();
+    Nodo* novo = new Nodo(p);
+    if(quant == 0){
+        head = novo;
+    }else{
+        novo->setProx(head);
+        head->setAnt(novo);
+        head = novo;
+    }
+    quant++;
+}
+
+PilhaDuplamenteEncadeada:: remove(){
+    if
+}
